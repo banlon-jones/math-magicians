175755fe4222b3c423b44f23050517afaa1f543a
banlonjones <banlonjones@go-groups.net>
2022-02-08 22:40:04 +0100

    calculator ui fix

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,9 +1,9 @@
 import React from 'react';
-// eslint-disable-next-line react/prefer-stateless-function
+
 class Calculator extends React.Component {
-  // eslint-disable-next-line no-useless-constructor
   constructor(props) {
     super(props);
+    this.state = { };
   }
 
   render() {
