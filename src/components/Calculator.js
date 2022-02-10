bd1bb2b9afaac0f338012c5d01193052de06cac4
banlonjones <banlonjones@go-groups.net>
2022-02-10 18:09:09 +0100

    calculator refactoring

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,62 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.eventHandle = this.eventHandle.bind(this);
-  }
+function Calculator() {
+  const [state, setState] = useState({
+    total: 0,
+    next: null,
+    operation: null,
+  });
 
-  eventHandle = (e) => {
-    const { next, total, operation } = calculate(this.state, e.target.value);
+  const eventHandle = (e) => {
+    const { next, total, operation } = calculate(state, e.target.value);
     if (next === null && total === null) {
-      this.setState({ next, total: '0', operation });
+      setState({ next, total: '0', operation });
     } else {
-      this.setState({ next, total, operation });
+      setState({ next, total, operation });
     }
   };
 
-  render() {
-    const { total, next } = this.state;
-    return (
-      <div className="container">
-        <div className="screen">
-          {
-          total ? (<div className="display">{total}</div>) : '0'
+  return (
+    <div className="container">
+      <div className="screen">
+        {
+          state.total ? (<div className="display">{state.total}</div>) : ''
           }
-          <div className="display">{next}</div>
-        </div>
-        <div className="buttons">
-          <div className="grid">
-            <button className="btn" value="AC" onClick={this.eventHandle} type="button">AC</button>
-            <button className="btn" value="+/-" onClick={this.eventHandle} type="button">+/-</button>
-            <button className="btn" value="%" onClick={this.eventHandle} type="button">%</button>
-            <button className="btn btn-orange" onClick={this.eventHandle} value="÷" type="button">÷</button>
-            <button className="btn" value="7" onClick={this.eventHandle} type="button">7</button>
-            <button className="btn" type="button" onClick={this.eventHandle} value="8">8</button>
-            <button className="btn" value="9" onClick={this.eventHandle} type="button">9</button>
-            <button className="btn btn-orange" onClick={this.eventHandle} value="x" type="button">x</button>
-            <button className="btn" value="4" onClick={this.eventHandle} type="button">4</button>
-            <button className="btn" value="5" onClick={this.eventHandle} type="button">5</button>
-            <button className="btn" value="6" onClick={this.eventHandle} type="button">6</button>
-            <button className="btn btn-orange" onClick={this.eventHandle} value="-" type="button">-</button>
-            <button className="btn" value="1" onClick={this.eventHandle} type="button">1</button>
-            <button className="btn" value="2" onClick={this.eventHandle} type="button">2</button>
-            <button className="btn" value="3" onClick={this.eventHandle} type="button">3</button>
-            <button className="btn btn-orange" onClick={this.eventHandle} value="+" type="button">+</button>
-            <button className="btn zero" onClick={this.eventHandle} value="0" type="button">0</button>
-            <button className="btn" onClick={this.eventHandle} value="." type="button">.</button>
-            <button className="btn btn-orange" value="=" onClick={this.eventHandle} type="button">=</button>
-          </div>
+        <div className="display">{state.next}</div>
+      </div>
+      <div className="buttons">
+        <div className="grid">
+          <button className="btn" value="AC" onClick={eventHandle} type="button">AC</button>
+          <button className="btn" value="+/-" onClick={eventHandle} type="button">+/-</button>
+          <button className="btn" value="%" onClick={eventHandle} type="button">%</button>
+          <button className="btn btn-orange" onClick={eventHandle} value="÷" type="button">÷</button>
+          <button className="btn" value="7" onClick={eventHandle} type="button">7</button>
+          <button className="btn" type="button" onClick={eventHandle} value="8">8</button>
+          <button className="btn" value="9" onClick={eventHandle} type="button">9</button>
+          <button className="btn btn-orange" onClick={eventHandle} value="x" type="button">x</button>
+          <button className="btn" value="4" onClick={eventHandle} type="button">4</button>
+          <button className="btn" value="5" onClick={eventHandle} type="button">5</button>
+          <button className="btn" value="6" onClick={eventHandle} type="button">6</button>
+          <button className="btn btn-orange" onClick={eventHandle} value="-" type="button">-</button>
+          <button className="btn" value="1" onClick={eventHandle} type="button">1</button>
+          <button className="btn" value="2" onClick={eventHandle} type="button">2</button>
+          <button className="btn" value="3" onClick={eventHandle} type="button">3</button>
+          <button className="btn btn-orange" onClick={eventHandle} value="+" type="button">+</button>
+          <button className="btn zero" onClick={eventHandle} value="0" type="button">0</button>
+          <button className="btn" onClick={eventHandle} value="." type="button">.</button>
+          <button className="btn btn-orange" value="=" onClick={eventHandle} type="button">=</button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Calculator;
