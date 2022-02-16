6c209841c4f794474730ce7408897b92c0d4b267
banlonjones <banlonjones@go-groups.net>
2022-02-16 14:19:17 +0100

    linter error fixed

--- src/test/navbar.component.test.js
+++ src/test/navbar.component.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen} from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import Navbar from '../components/navbar/navbar';
 
 describe('navbar component test', () => {
